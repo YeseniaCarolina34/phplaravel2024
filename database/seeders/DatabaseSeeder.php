9536c23761a0d916849fda35da21b9aeb6164519
MINEDUCYT <MINEDUCYT@YESENIA>
2024-07-02 22:07:47 -0600

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+
+        DB::table('docente')->insert([
+            'nombre' => 'adminn',
+            'apellido' => 'adminn',
+            'email' => 'admin@adminn.com',
+            'password' => Hash::make('adminn'),
+        ]);
+    }
+}
